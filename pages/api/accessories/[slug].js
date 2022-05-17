0d5a2c47e02e6102ab240e2c7030dc89637d73f0
beesayers <blsayers13@gmail.com>
2022-05-16 23:48:49 -0400

    Restrict POSTs.

--- pages/api/accessories/[slug].js
+++ pages/api/accessories/[slug].js
@@ -2,5 +2,10 @@ const {accessories} = require("./data.json");
 
 export default (req, res) => {
   const singleAccessory = accessories.filter((item) => item.slug === req.query.slug);
-  res.status(200).json(singleAccessory);
+  if(req.method === "GET") {
+    res.status(200).json(singleAccessory)
+  } else {
+    res.setHeader("Allow", ["GET"]);
+    res.status(405).json({message: `Method ${req.method} is not allowed.`})
+  }
 }
--- pages/api/accessories/index.js
+++ pages/api/accessories/index.js
@@ -3,5 +3,11 @@
 const {accessories} = require("./data.json");
 
 export default function handler(req, res) {
-  res.status(200).json(accessories)
+  if(req.method === "GET") {
+    res.status(200).json(accessories)
+  } else {
+    res.setHeader("Allow", ["GET"]);
+    res.status(405).json({message: `Method ${req.method} is not allowed.`})
+  }
+  
 }
