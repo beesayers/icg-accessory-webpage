de559438848d98c5d8b5a6a4e68b12a514d38d59
beesayers <blsayers13@gmail.com>
2022-05-24 23:43:48 -0400

    Added VERCEL_URL to config

--- config/index.js
+++ config/index.js
@@ -1,1 +1,1 @@
-export const API_URL = process.env.API_URL ? process.env.API_URL : "http://localhost:3000";
+export const API_URL = process.env.VERCEL_URL ? `${process.env.VERCEL_URL}/api` : "http://localhost:3000";
